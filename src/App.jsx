88a6f3a787c3b2664d324e7013896d62b54a8c43
marciolucasjunior <109992150+marciolucasjunior@users.noreply.github.com>
2023-09-18 20:29:12 -0300

    inicio do projeto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './index.css'
+import Tabela from './components/Tabela/Tabela'
+
+function App() {
+
+
+  return (
+    <>
+      <Tabela />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Tabela/Tabela.jsx
@@ -0,0 +1,14 @@
+
+import './Tabela.css'
+
+
+function Tabela(){
+
+    return(
+        <div>
+            <h1>Tabela</h1>
+        </div>
+    )
+}
+
+export default Tabela
